188198128a0d02f488083813a7199a3f46fb00f7
david <daveaneo@gmail.com>
2023-03-16 10:52:50 -0600

    added rejuve info

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ WALLET_ADDRESS = os.getenv('WALLET_ADDRESS')
 AGIX_USDT_CONTRACT_ADDRESS= os.getenv('AGIX_USDT_CONTRACT_ADDRESS')
 ETH_USDT_CONTRACT_ADDRESS= os.getenv('ETH_USDT_CONTRACT_ADDRESS')
 AGIX_ETH_CONTRACT_ADDRESS= os.getenv('AGIX_ETH_CONTRACT_ADDRESS')
+RJV_ETH_CONTRACT_ADDRESS= os.getenv('RJV_ETH_CONTRACT_ADDRESS')
 
 YIELD_VAULT_CONTRACT_ADDRESS= os.getenv('YIELD_VAULT_CONTRACT_ADDRESS')
 
 AGIX_CONTRACT_ADDRESS= os.getenv('AGIX_CONTRACT_ADDRESS')
 USDT_CONTRACT_ADDRESS= os.getenv('USDT_CONTRACT_ADDRESS')
 ETH_CONTRACT_ADDRESS= os.getenv('ETH_CONTRACT_ADDRESS')
+RJV_CONTRACT_ADDRESS= os.getenv('RJV_CONTRACT_ADDRESS')
 
 RPC = os.getenv('RPC')
 CHAIN_ID = int(str(os.getenv('CHAIN_ID')))
@@ -38,10 +40,11 @@ my_provider = Web3(Web3.HTTPProvider(RPC))
 my_provider.eth.set_gas_price_strategy(rpc_gas_price_strategy)
 
 # create contract instance from address and abi
-
 agix_usdt_pair_contract = my_provider.eth.contract(address=AGIX_USDT_CONTRACT_ADDRESS, abi=PAIR_ABI)
 eth_usdt_pair_contract = my_provider.eth.contract(address=ETH_USDT_CONTRACT_ADDRESS, abi=PAIR_ABI)
 agix_eth_pair_contract = my_provider.eth.contract(address=AGIX_ETH_CONTRACT_ADDRESS, abi=PAIR_ABI)
+rjv_eth_pair_contract = my_provider.eth.contract(address=RJV_ETH_CONTRACT_ADDRESS, abi=PAIR_ABI)
+
 yield_vault_contract = my_provider.eth.contract(address=YIELD_VAULT_CONTRACT_ADDRESS, abi=YIELD_VAULT_ABI)
 
 
@@ -49,9 +52,16 @@ decimals = {
     "usdt": 6,
     "eth": 18,
     "agix": 8,
+    "rjv": 6,
 }
 
-def get_my_returns(my_wallet):
+
+
+
+
+def get_my_agix_returns(my_wallet):
+    print(f'\n########################################')
+    print(f'##########    AGIX     ################')
     ###################################################
     # get prices
     reserves = agix_usdt_pair_contract.functions.getReserves().call()
@@ -107,61 +117,72 @@ def get_my_returns(my_wallet):
     print(f'  ETH-AGIX: {round(my_lp/total_lp*100, 2):,} %')
 
 
-
-# these return percents out of 100
-def get_APY():
-    # (USDC) Lending Contract
-    total_usdc_stake = USDC_lending_contract.functions.totalDepositedUsdc().call() / 10 ** decimals["usdc"]
-
-    harvest_info = USDC_lending_contract.functions.harvestInfo().call()
-    total_yield_USDC = harvest_info[1] / 10 ** decimals["usdc"]
-    rewardRate = harvest_info[0]  # seconds
-
-    # (GSD) Staking Contract
-    total_gsd_stake = GSD_staking_contract.functions.aggregateTVL().call() / 10 ** decimals['gsd']  # aggregate_TVL
-
-    # Pair Contract for GSD
-    reserves = pair_contract.functions.getReserves().call()
-    #    gsd_price = 0  # pair_contract.functions.getReserves().call()
-    token_zero = pair_contract.functions.token0().call()
-
-    # get price per gsd wei
-    if token_zero == GSD_CONTRACT_ADDRESS:
-        gsd_price = (reserves[1] / 10 ** decimals['usdc']) / (reserves[0] / 10 ** decimals['gsd'])
+def get_my_rjv_returns(my_wallet):
+    print(f'\n########################################')
+    print(f'##########    REJUVE     ################')
+    ###################################################
+    # get prices
+    reserves = rjv_eth_pair_contract.functions.getReserves().call()
+    token_zero = rjv_eth_pair_contract.functions.token0().call()
+    if token_zero == RJV_CONTRACT_ADDRESS:
+        rjv_price_in_eth = (reserves[1] / 10 ** decimals['eth']) / (reserves[0] / 10 ** decimals['rjv'])
     else:
-        gsd_price = (reserves[0] / 10 ** decimals['usdc']) / (reserves[1] / 10 ** decimals['gsd'])
+        rjv_price_in_eth = (reserves[0] / 10 ** decimals['eth']) / (reserves[1] / 10 ** decimals['rjv'])
 
-    # ts stores the time in seconds
-    # ts = time.time()
 
-    seconds_in_year = 365 * 24 * 60 * 60
+    # Get ETH Price
+    reserves = eth_usdt_pair_contract.functions.getReserves().call()
+    token_zero = eth_usdt_pair_contract.functions.token0().call()
+    if token_zero == ETH_CONTRACT_ADDRESS:
+        eth_price = (reserves[1] / 10 ** decimals['usdt']) / (reserves[0] / 10 ** decimals['eth'])
+    else:
+        eth_price = (reserves[0] / 10 ** decimals['usdt']) / (reserves[1] / 10 ** decimals['eth'])
 
-    totalYieldInDollars = total_yield_USDC
-    totalYieldFromStakeInUSD = totalYieldInDollars * 0.05  # 5% of USDC Lending yield
-    totalGSDStakedValueInUSD = total_gsd_stake * gsd_price
-    totalUsdcStakeInUSD = total_usdc_stake
 
-    # Lending
-    periodInterestRateLending = 1 if totalUsdcStakeInUSD == 0 else (1 + totalYieldInDollars * 0.9 / totalUsdcStakeInUSD)
-    periodBaseLending = periodInterestRateLending
+    rjv_price = rjv_price_in_eth *eth_price
 
-    # Staking
-    periodInterestRateStaking = 1 if totalGSDStakedValueInUSD == 0 else ( totalYieldFromStakeInUSD / totalGSDStakedValueInUSD) + 1
-    periodBaseStaking = periodInterestRateStaking
 
-    yearly_apy_lending = 100 * (periodBaseLending ** (seconds_in_year / rewardRate)) - 100
-    yearly_apy_staking = 100 * (periodBaseStaking ** (seconds_in_year / rewardRate)) - 100
+    #################################################
+    # Get total tokens for each in lp
+    total_rjv = 0
+    total_eth = 0
+    # AGIX Vault
+    # my_lp, _ = yield_vault_contract.functions.userInfo(2, my_wallet).call()
 
-    # these return percents out of 100
-    return {
-        "gsd_apy": None if totalUsdcStakeInUSD == 0 else yearly_apy_staking,
-        "usdc_apy": None if totalGSDStakedValueInUSD == 0 else yearly_apy_lending
-    }
+    # Uniswap Pair
+    reserves = rjv_eth_pair_contract.functions.getReserves().call()
+    token_zero = rjv_eth_pair_contract.functions.token0().call()
 
-# get_APY()
+    my_lp = rjv_eth_pair_contract.functions.balanceOf(my_wallet).call()
+    total_lp = rjv_eth_pair_contract.functions.totalSupply().call()
+
+    if token_zero == RJV_CONTRACT_ADDRESS:
+        total_rjv += reserves[0]*my_lp/total_lp/(10**decimals["rjv"])
+        total_eth += reserves[1]*my_lp/total_lp/(10**decimals["eth"])
+    else:
+        total_rjv += reserves[1]*my_lp/total_lp/(10**decimals["rjv"])
+        total_eth += reserves[0]*my_lp/total_lp/(10**decimals["eth"])
+
+    print(f'Total Assets')
+    print(f'  ETH: {round(total_eth, 2):,}')
+    print(f'  RJV: {round(total_rjv, 2):,}')
+
+    print(f'Prices')
+    print(f'  ETH: ${round(eth_price, 2):,}')
+    print(f'  RJV: ${round(rjv_price, 2):,}')
+
+    print(f'Asset Value')
+    print(f'  ETH: ${round(total_eth*eth_price, 2):,}')
+    print(f'  RJV: ${round(total_rjv*rjv_price, 2):,}')
+    print(f'  Combined: ${round(total_eth*eth_price+total_rjv*rjv_price, 2):,}')
+
+    print(f'Percent Of Liquidity Pool')
+    print(f'  ETH-RJV: {round(my_lp/total_lp*100, 2):,} %')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_my_returns(WALLET_ADDRESS)
+    get_my_agix_returns(WALLET_ADDRESS)
+    get_my_rjv_returns(WALLET_ADDRESS)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
