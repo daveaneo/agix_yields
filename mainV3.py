d45a4178073a8b955b7d35bf322def73d449ce32
david <daveaneo@gmail.com>
2024-01-11 10:39:52 -0700

    added logic for unbonded staking

--- mainV3.py
+++ mainV3.py
@@ -11,9 +11,6 @@ from datetime import datetime
 load_dotenv()
 WALLET_ADDRESS = os.getenv('WALLET_ADDRESS')
 
-# todo -- consider our JSON file and issues we may have when having multiple
-# tokens and double counting them, like SDAO. Currently, this won't happen.
-
 
 class TokenYield:
     def __init__(self, token_info: Dict[str, Any]):
@@ -24,10 +21,8 @@ class TokenYield:
         token_info (Dict[str, Any]): A dictionary containing token and blockchain information.
         """
         self.token_info = token_info
-        self.decompress_token_info()
         self.load_web3()
-        if not self.web3:
-            print(f'how do we handle this?')
+        self.decompress_token_info()
         self.load_ABIs() # could be done in parent class
         self.fetch_token_data()
         self.calculate_totals()
@@ -59,19 +54,22 @@ class TokenYield:
 
 
     def decompress_token_info(self) -> None:
+        bonded_staking_address = self.token_info.get("bondedStaking", {}).get("contractAddress", None)
+        unbonded_staking_address = self.token_info.get("unbondedStaking", {}).get("contractAddress", None)
+
         self.name = self.token_info.get("name")
         self.symbol = self.token_info.get("symbol")
         self.decimals = self.token_info.get("decimals")
         self.blockchain = self.token_info.get('blockchain')
         self.token_address = self.checksum_address(self.token_info.get("tokenAddress"))
-        self.bonded_staking_address = self.checksum_address(self.token_info.get("bondedStakingAddress"))
-        self.unbonded_staking_address = self.checksum_address(self.token_info.get("unbondedStakingAddress"))
+        self.bonded_staking_address = self.checksum_address(bonded_staking_address)
+        self.unbonded_staking_address = self.checksum_address(unbonded_staking_address)
         self.liquidity_pool_info_list = self.token_info.get("liquidityPool", [{}])
         self.in_wallet = self.token_info.get("inWallet")
 
     def load_web3(self) -> None:
         blockchain = self.token_info.get("blockchain")
-        web3 = None
+        self.web3 = None
         if blockchain == "Ethereum":
             rpc_url = os.getenv('ETH_RPC')
         elif blockchain == "Binance":
@@ -86,8 +84,6 @@ class TokenYield:
         if not self.web3:
             print(f'unsupported blockchain')
 
-
-
     def load_ABIs(self) -> None:
         # Load ABI files (assuming these are stored in a directory named 'ABI')
         with open('ABI/pair.json', 'r') as file:
@@ -96,12 +92,15 @@ class TokenYield:
             self.YIELD_VAULT_ABI = json.load(file)
         with open('ABI/ERC20.json', 'r') as file:
             self.ERC20_ABI = json.load(file)
+        with open('ABI/unbondedStaking.json', 'r') as file:
+            self.UNBONDED_STAKING_ABI = json.load(file)
+
 
     def print_data(self) -> None:
         """
         Prints the token data in a formatted manner.
         """
-        print(f'... {self.symbol} data ...')
+        print(f'... printing {self.symbol} data ...')
         # print(json.dumps(self.token_info, indent=4))
 
     def get_wallet_balance(self) -> None:
@@ -116,14 +115,19 @@ class TokenYield:
         Fetches and returns bonded data.
         """
         # Implement logic to fetch bonded data
-        return {}
+        return
 
     def get_unbonded(self) -> None:
         """
         Fetches and returns unbonded data.
         """
         # Implement logic to fetch unbonded data
-        return {}
+        unbonded_contract = self.web3.eth.contract(address=self.unbonded_staking_address, abi=self.UNBONDED_STAKING_ABI)
+        amount, user_debt = unbonded_contract.functions.userInfo(0, WALLET_ADDRESS).call()
+        self.token_info['unbondedStaking']['staked'] = amount / 10 ** self.decimals
+        # todo -- understand the logic for getting the rewards
+
+        return
 
     def get_yield(self) -> None:
         """
@@ -179,8 +183,6 @@ class TokenYield:
             lp_dict['pairedAssetAmountNow'] = total_paired_token
             lp_dict['pendingRewardsFromYieldVault'] = 0 # todo
 
-            print(f'fraction of pool: {my_lp / total_lp * 100:.2f}%')
-
         return my_token_data
 
     def get_all(self) -> None:
@@ -223,7 +225,8 @@ class TokenYield:
 
     # @staticmethod
     # def checksum_address(address: str) -> str:
-    def checksum_address(self, address: str) -> str:
+    @staticmethod
+    def checksum_address(address: str) -> str:
         """
         Converts an Ethereum address to its checksummed version.
 
@@ -233,7 +236,9 @@ class TokenYield:
         Returns:
         str: The checksummed Ethereum address.
         """
-        return self.web3.utils.toChecksumAddress(address)
+        return address if not address else Web3.to_checksum_address(address)
+        # return Web3.to_checksum_address(address)
+        # return self.web3.utils.toChecksumAddress(address)
 
 
 # # used for getting prices (in terms of each other)
@@ -277,7 +282,7 @@ def print_group_data(results: Dict):
     """
     Prints grouped data and uses print_individual to print each token's data.
     """
-    print(f'printing group data... todo -- please impliment holder class')
+    print(f'printing group data...  -- please impliment holder class') # todo
     # for symbol, data in results.items():
     #     print_individual(data)
 
