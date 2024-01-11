4d01f73c5c336fd98e908c46796e82b6e4e44116
david <daveaneo@gmail.com>
2024-01-10 18:34:24 -0700

    updated json, adding more tokens flushed out functions for fetch_token_data, making several helper functions.

--- mainV3.py
+++ mainV3.py
@@ -11,6 +11,9 @@ from datetime import datetime
 load_dotenv()
 WALLET_ADDRESS = os.getenv('WALLET_ADDRESS')
 
+# todo -- consider our JSON file and issues we may have when having multiple
+# tokens and double counting them, like SDAO. Currently, this won't happen.
+
 
 class TokenYield:
     def __init__(self, token_info: Dict[str, Any]):
@@ -23,8 +26,11 @@ class TokenYield:
         self.token_info = token_info
         self.decompress_token_info()
         self.load_web3()
+        if not self.web3:
+            print(f'how do we handle this?')
         self.load_ABIs() # could be done in parent class
         self.fetch_token_data()
+        self.calculate_totals()
         self.print_data()
 
     def fetch_token_data(self) -> Dict[str, Any]:
@@ -61,7 +67,7 @@ class TokenYield:
         self.bonded_staking_address = self.token_info.get("bondedStakingAddress")
         self.unbonded_staking_address = self.token_info.get("unbondedStakingAddress")
         self.liquidity_pool_info_list = self.token_info.get("liquidityPool", [{}])
-
+        self.in_wallet = self.token_info.get("inWallet")
 
     def load_web3(self) -> None:
         blockchain = self.token_info.get("blockchain")
@@ -70,6 +76,9 @@ class TokenYield:
             rpc_url = os.getenv('ETH_RPC')
         elif blockchain == "Binance":
             rpc_url = os.getenv('BNB_RPC')
+        else:
+            print(f'unable to find rpc for blockchain: {blockchain}')
+            return
 
         # Setup Web3 connection
         self.web3 = Web3(Web3.HTTPProvider(rpc_url))
@@ -92,16 +101,15 @@ class TokenYield:
         """
         Prints the token data in a formatted manner.
         """
-        print(f'printing....')
-        print(json.dumps(self.token_info, indent=4))
+        print(f'... {self.symbol} data ...')
+        # print(json.dumps(self.token_info, indent=4))
 
     def get_wallet_balance(self) -> None:
         """
         Fetches and returns bonded data.
         """
         token_contract = self.web3.eth.contract(address=self.token_address, abi=self.ERC20_ABI)
-        bal = token_contract.functions.balanceOf(WALLET_ADDRESS).call()
-        print(f'wallet balance: {bal}')
+        bal = token_contract.functions.balanceOf(WALLET_ADDRESS).call() / 10**token_contract.functions.decimals().call()
 
     def get_bonded(self) -> None:
         """
@@ -167,6 +175,10 @@ class TokenYield:
             my_token_data[self.symbol] = my_token_data.get(self.symbol, 0) + total_token
             my_token_data[paired_token_symbol] = my_token_data.get(paired_token_symbol, 0) + total_paired_token
 
+            lp_dict['mainAssetAmountNow'] = total_token
+            lp_dict['pairedAssetAmountNow'] = total_paired_token
+            lp_dict['pendingRewardsFromYieldVault'] = 0 # todo
+
             print(f'fraction of pool: {my_lp / total_lp * 100:.2f}%')
 
         return my_token_data
@@ -175,6 +187,7 @@ class TokenYield:
         """
         Calls all data-fetching methods and aggregates the results.
         """
+        print(f'get_all called. We should remove this function in the future.')
         return {
             "bonded": self.get_bonded(),
             "unbonded": self.get_unbonded(),
@@ -190,6 +203,24 @@ class TokenYield:
             "current_datetime": datetime.now().isoformat(),
             "data": self.data
         }
+
+    def calculate_totals(self) -> None:
+        """
+        Fetches and returns bonded data.
+        """
+        # Implement logic to fetch bonded data
+        bonded = self.token_info.get('bondedStaking').get('staked', 0) + self.token_info.get('bondedStaking').get('pendingRewards', 0)
+        unbonded = self.token_info.get('unbondedStaking').get('staked') + self.token_info.get('unbondedStaking').get('pendingRewards', 0)
+        totals = dict({self.symbol: self.token_info.get('inWallet', 0) + bonded + unbonded})
+
+        for lp_dict in self.liquidity_pool_info_list:
+            totals[self.symbol] += lp_dict.get("mainAssetAmountNow")
+            paired_token_symbol = lp_dict.get("pairedTokenSymbol")
+            paired_amount = lp_dict.get("pairedAssetAmountNow")
+            totals[paired_token_symbol] = totals.get(paired_token_symbol,0) + paired_amount
+
+        self.token_info['totalAssetsOwned'] = totals
+
 
 
 # # used for getting prices (in terms of each other)
@@ -221,19 +252,21 @@ def main():
             token = futures[future]
             try:
                 token_yield_instance = future.result()
-                results[token['symbol']] = token_yield_instance.get_all()
+                # results[token['symbol']] = token_yield_instance.get_all()
             except Exception as exc:
                 print(f'{token["symbol"]} generated an exception: {exc}')
 
     # Print the results in series
-    print_group_data(results)
+    # todo -- have container class print all
+    # print_group_data(results)
 
 def print_group_data(results: Dict):
     """
     Prints grouped data and uses print_individual to print each token's data.
     """
-    for symbol, data in results.items():
-        print_individual(data)
+    print(f'printing group data... todo -- please impliment holder class')
+    # for symbol, data in results.items():
+    #     print_individual(data)
 
 def print_individual(data: Dict):
     """
